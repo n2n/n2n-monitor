5a5a0e6d0d490b4360e5a5195e71a80bdb2ecba0
andreasvonburg <a@von-burg.net>
2023-01-09 22:12:27 +0100

    n2n 7.4 implementations

---
+++ src/app/n2n/monitor/alert/AlertException.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace n2n\monitor\alert;
+
+use n2n\util\ex\LogInfo;
+use Throwable;
+
+class AlertException extends \RuntimeException implements LogInfo {
+
+	function __construct(private ?string $hashCode = null, string $message = null, int $code = null,
+			?Throwable $previous = null) {
+		parent::__construct((string) $message, (int) $code, $previous);
+	}
+
+	private ?string $logMessage = null;
+
+	function hashCode(): ?string {
+		return $this->hashCode;
+	}
+
+	function setLogMessage(?string $logMessage) {
+		$this->logMessage = $logMessage;
+	}
+
+	function getLogMessage(): ?string {
+		return $this->logMessage;
+	}
+}
