fc8979f534e006c8f329179a669d9b32f6d50f06
nikolai <nikolai@schmid.guru>
2023-05-11 11:45:36 -0300

    MonitorHtmlBuilder for easy setup

---
+++ src/app/n2n/monitor/ui/MonitorHtmlBuilder.php
@@ -0,0 +1,19 @@
+<?php
+namespace n2n\monitor\ui;
+
+use n2n\impl\web\ui\view\html\HtmlView;
+
+/**
+ * MonitorHtmlBuilderMeta provides non-html meta information to your views. You can access it over
+ */
+class MonitorHtmlBuilder {
+	public function __construct(HtmlView $view) {
+		$this->view = $view;
+	}
+
+	public function setup() {
+		$htmlMeta = $this->view->getHtmlBuilder()->meta();
+		$htmlMeta->addMeta(['name' => 'monitor-url', 'content' => $this->view->getN2nContext()->getMonitor()->getAlertPostUrl()]);
+		$htmlMeta->addJs('monitor.min.js', 'n2n\monitor', true, true, ['defer']);
+	}
+}
